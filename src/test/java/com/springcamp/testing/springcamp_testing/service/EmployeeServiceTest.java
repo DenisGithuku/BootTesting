c26f3a06ef7578242577f7d6ae881e6ec1eae268
githukudenis <githukudenis@gmail.com>
2025-04-19 23:34:16 +0300

    Test save employee throws error on duplicate insertion

--- src/test/java/com/springcamp/testing/springcamp_testing/service/EmployeeServiceTest.java
+++ src/test/java/com/springcamp/testing/springcamp_testing/service/EmployeeServiceTest.java
@@ -1,5 +1,6 @@
 package com.springcamp.testing.springcamp_testing.service;
 
+import com.springcamp.testing.springcamp_testing.exception.ResourceNotFoundException;
 import com.springcamp.testing.springcamp_testing.model.Employee;
 import com.springcamp.testing.springcamp_testing.repository.EmployeeRepository;
 import com.springcamp.testing.springcamp_testing.service.impl.EmployeeServiceImpl;
@@ -7,16 +8,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
@@ -31,12 +34,7 @@ public class EmployeeServiceTest {
 
     @BeforeEach
     public void setup() {
-        employee = Employee.builder()
-                .id(1L)
-                .firstName("Denis")
-                .lastName("Githuku")
-                .email("denisgithuku@gmail.com")
-                .build();
+        employee = Employee.builder().id(1L).firstName("Denis").lastName("Githuku").email("denisgithuku@gmail.com").build();
     }
 
     // Junit test for save employee
@@ -53,5 +51,20 @@ public class EmployeeServiceTest {
 
         // then - verify output
         assertThat(savedEmployee).isNotNull();
+    }
+
+    // Junit test for save employee throws exception
+    @DisplayName("Junit test for save employee throws exception")
+    @Test
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException() {
+        // given - precondition for setup
+
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.of(employee));
+
+        // when - action or behaviour to test
+        assertThrows(ResourceNotFoundException.class, () -> employeeService.saveEmployee(employee));
+
+        // then - verify output
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 }
